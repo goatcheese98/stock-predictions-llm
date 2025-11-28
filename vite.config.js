2c98c300e3d3ac959966bbb1ab8bbd1f897a80e2
Rohan Jasani <jasani.rohan@gmail.com>
2025-11-28 13:19:14 -0800

    feat: Update Vite base path and Open Graph URLs for GitHub Pages deployment.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig(({ command, mode }) => {
 
 	return {
 		plugins: [],
-		base: '/',
+		base: '/stock-predictions-llm/',
 		build: {
 			outDir: 'dist',
 			assetsDir: 'assets',
